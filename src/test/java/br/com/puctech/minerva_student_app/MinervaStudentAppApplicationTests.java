b763e7df22df6a809481ab63eb0e383b5a3c06a3
Luis-coelho30 <luis.coelho.761@gmail.com>
2025-09-01 15:38:18 -0300

    Iniciando o projeto Minerva

---
+++ src/test/java/br/com/puctech/minerva_student_app/MinervaStudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.puctech.minerva_student_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinervaStudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

463d6d6d8906844cf618677c4a066835d08a9542
Kaua Bezerra <kaua.bezerra2006@gmail.com>
2025-09-11 19:02:53 -0300

    DisciplinaDTO

---
+++ student-app/src/main/java/br/com/puctech/minerva_student_app/dto/DisciplinaDTO.java
@@ -0,0 +1,26 @@
+package br.com.puctech.minerva_student_app.dto;
+
+import br.com.puctech.minerva_student_app.model.Disciplina;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DisciplinaDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Boolean arquivada;
+    private Double mediaNecessaria;
+    private Double mediaAtual;
+    private Integer creditos;
+    private Integer faltasRestantes;
+
+    public DisciplinaDTO(Disciplina disciplina) {
+        BeanUtils.copyProperties(disciplina, this);
+    }
+}

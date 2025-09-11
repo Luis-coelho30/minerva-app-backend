b8ab1e37a0259ebdbb33fe61b9c342cb423861ab
Kaua Bezerra <kaua.bezerra2006@gmail.com>
2025-09-11 14:47:47 -0300

    Nota Repository

--- student-app/src/main/java/br/com/puctech/minerva_student_app/repo/NotaRepository.java
+++ student-app/src/main/java/br/com/puctech/minerva_student_app/repo/NotaRepository.java
@@ -1,10 +1,10 @@
 package br.com.puctech.minerva_student_app.repo;
 
-import br.com.puctech.minerva_student_app.model.Tarefa;
+import br.com.puctech.minerva_student_app.model.Nota;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+public interface NotaRepository extends JpaRepository<Nota, Long> {
 
 }

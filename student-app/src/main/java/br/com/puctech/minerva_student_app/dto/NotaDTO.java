10dc1ceb115b887546b078d4b6522bd89a6aa199
Kaua Bezerra <kaua.bezerra2006@gmail.com>
2025-09-11 18:57:16 -0300

    NotaDTO

---
+++ student-app/src/main/java/br/com/puctech/minerva_student_app/dto/NotaDTO.java
@@ -0,0 +1,22 @@
+package br.com.puctech.minerva_student_app.dto;
+
+import br.com.puctech.minerva_student_app.model.Nota;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NotaDTO {
+
+    private Long id;
+    private String descricao;
+    private Double valor;
+    private Integer peso;
+    
+    private NotaDTO(Nota nota) {
+        BeanUtils.copyProperties(nota, this);
+    }
+}
